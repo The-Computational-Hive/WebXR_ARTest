4ede8a683756050e873d0477d1f3f4e07fdb7bb9
Andrea Rossi <a.rossi.andrea@gmail.com>
2026-03-27 16:27:05 +0100

    add .gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
+import { defineConfig } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
-export default {
+export default defineConfig({
   plugins: [basicSsl()],
   server: {
     https: true,
-    host: true, // exposes on local network (0.0.0.0)
+    host: true,
   },
-};
+});
